2886e1543f79cde9f8775e537fb97820793bf25d
Stuler <peter.jurek@gmail.com>
2024-09-12 07:12:18 +0200

    Feat: basic admin section layout and presenters init.

---
+++ app/UI/Modules/Admin/Advertisement/AdvertisementPresenter.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+namespace App\UI\Modules\Admin\Advertisement;
+
+use App\UI\Modules\Admin\BaseAdminPresenter;
+
+final class AdvertisementPresenter extends BaseAdminPresenter
+{
+
+	public function renderDefault(?string $type = null)
+	{
+
+	}
+
+}

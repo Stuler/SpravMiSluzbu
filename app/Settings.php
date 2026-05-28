95889c43097d5dfe94b22d5ec7cd2840990c7405
stuly <peter.jurek@gmail.com>
2026-05-28 11:07:56 +0200

    Feat: Refactor test configuration by removing unused parameters and simplifying settings

--- app/Settings.php
+++ app/Settings.php
@@ -8,9 +8,7 @@ readonly class Settings
 	public function __construct(
 		public bool   $debugMode,
 		public string $appDir,
-		public string $wwwDir,
-		public string $testPassword,
-		public string $testUrl
+		public string $wwwDir
 	)
 	{
 	}
--- app/UI/Modules/Base/BasePresenter.php
+++ app/UI/Modules/Base/BasePresenter.php
@@ -2,15 +2,12 @@
 
 namespace App\UI\Modules\Base;
 
-use App\Model\App;
 use App\Model\Latte\TemplateProperty;
 use App\Model\Security\SecurityUser;
-use App\Settings;
 use App\UI\Control\TFlashMessage;
 use App\UI\Control\TModuleUtils;
 use Contributte\Application\UI\Presenter\StructuredTemplates;
 use Nette\Application\UI\Presenter;
-use Nette\DI\Attributes\Inject;
 use Nette\Http\Session;
 
 /**
@@ -26,32 +23,9 @@ abstract class BasePresenter extends Presenter
 
 	public Session $session;
 
-	public function __construct(
-		private readonly Settings $settings,
-
-	)
-	{
-		parent::__construct();
-	}
-
 	protected function startup(): void
 	{
 		parent::startup();
 		$this->session = $this->getSession();
-	}
-
-	public function beforeRender(): void
-	{
-		// Get the host from the HTTP request
-		$host = $this->getHttpRequest()->getUrl()->getHost();
-
-		// Check the current presenter and action to avoid infinite redirect
-		// get access_granted setting from session
-		$section = $this->getSession('test_access');
-		$accessGranted = $section->get('access_granted');
-		if ($host === $this->settings->testUrl && $accessGranted !== true) {
-			// Redirect to the TestAuthPresenter:default action
-			$this->redirect(App::DESTINATION_TEST_ACCESS);
-		}
 	}
 }
